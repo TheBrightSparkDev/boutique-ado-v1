5a4f6baf51dc37ac6b5490ef47c2bc76a426983c
TheBrightSparkDev <danharriesxd@gmail.com>
2022-05-09 16:38:33 +0000

    feat - added a stripe payment system

--- products/models.py
+++ products/models.py
@@ -1,12 +1,11 @@
 from django.db import models
 
-# Create your models here.
 
 class Category(models.Model):
 
     class Meta:
-        verbose_name_plural = 'categories'
-
+        verbose_name_plural = 'Categories'
+        
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -28,7 +27,5 @@ class Product(models.Model):
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
-
     def __str__(self):
         return self.name
-        
