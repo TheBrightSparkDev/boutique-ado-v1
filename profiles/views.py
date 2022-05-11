dd31db8ccc3209a0d35016fa896fc749f74886a8
TheBrightSparkDev <danharriesxd@gmail.com>
2022-05-11 17:00:57 +0000

    feat - completed the product admin section

--- profiles/views.py
+++ profiles/views.py
@@ -4,8 +4,9 @@ from django.contrib import messages
 from .models import UserProfile
 from .forms import UserProfileForm
 from checkout.models import Order
+from django.contrib.auth.decorators import login_required
 
-
+@login_required
 def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
@@ -15,8 +16,10 @@ def profile(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Profile updated successfully')
-
-    form = UserProfileForm(instance=profile)
+        else:
+            messages.error(request, 'Update failed. please ensure the form is valid')
+    else:
+        form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
 
     template = 'profiles/profile.html'
