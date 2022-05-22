22c67e8a0bc7e34e3b85bb9f1da36f447c156853
TheBrightSparkDev <danharriesxd@gmail.com>
2022-05-22 22:20:54 +0000

    still not working

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -172,7 +172,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-AWS_DEFAULT_REGION=eu-west-2
+AWS_DEFAULT_REGION='eu-west-2'
 
 if 'USE_AWS' in os.environ:
     AWS_STORAGE_BUCKET_NAME = 'tbs-boutique-ado',
