2728f7c44db820fd1391e8913ac1eda3cd9a78ab
TheBrightSparkDev <danharriesxd@gmail.com>
2022-05-23 12:40:19 +0000

    feat - fingers crossed the tutor has smashed it

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -181,9 +181,9 @@ if 'USE_AWS' in os.environ:
         'CacheControl': 'max-age=94608000',
     }
 
-    AWS_STORAGE_BUCKET_NAME = 'tbs-boutique-ado',
-    AWS_S3_REGION_NAME = 'eu-west-2',
-    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID'),
+    AWS_STORAGE_BUCKET_NAME = 'tbs-boutique-ado'
+    AWS_S3_REGION_NAME = 'eu-west-2'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
